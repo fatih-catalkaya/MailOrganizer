c9a8c0e635850193255ce817f7b62b8af9fe9cc8
Fatih Çatalkaya <fatih.catalkaya@yahoo.de>
2022-11-15 22:33:50 +0100

    Implemented execution loop

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,17 @@
 import configuration.Configuration;
 import configuration.MailAccount;
+import mail.MailOrganizer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class Main {
+
     public static void main(String[] args){
         MailAccount[] mailAccounts = null;
         try{
@@ -9,8 +19,14 @@ public class Main {
         }
         catch (Exception ex){
             ex.printStackTrace();
+            return;
         }
 
-        System.out.println("Hallo");
+        List<CompletableFuture<Void>> cfl = new ArrayList<>(mailAccounts.length);
+        for (final MailAccount mailAccount : mailAccounts) {
+            CompletableFuture<Void> completableFuture = CompletableFuture.runAsync(() -> MailOrganizer.organizeMailAccount(mailAccount));
+            cfl.add(completableFuture);
+        }
+        CompletableFuture.allOf(cfl.toArray(new CompletableFuture[0])).join();
     }
 }
---
+++ src/main/java/mail/MailOrganizer.java
@@ -0,0 +1,9 @@
+package mail;
+
+import configuration.MailAccount;
+
+public class MailOrganizer {
+    public static void organizeMailAccount(final MailAccount mailAccount){
+        // TODO: Implement me
+    }
+}
